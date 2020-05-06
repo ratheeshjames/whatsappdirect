bcebe5fb657a0755cca50f9ef270fb6274cb7eab
ratheeshjames <54690349+ratheeshjames@users.noreply.github.com>
2020-05-06 16:56:19 +0530

    pwa fix

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 var $$ = Dom7;
-
+let deferredPrompt;
+      window.addEventListener('beforeinstallprompt', (e) =>{
+        e.preventDefault();
+        deferredPrompt = e;
+        setTimeout(function () {                
+          $$("#dummyBtn").trigger("click");
+      }, 1000); 
+      $$('#installBtn').on('click', function (e) {
+        $$(".pwa-sheet-swipe-to-close").hide();
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if(choiceResult.outcome == 'accepted'){
+            console.log('User accepted the A2HS prompt');
+          }else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+    });
+      });  
 var app = new Framework7({
   root: "#app", // App root element
 
   name: "Whatsapp Direct V2", // App name
   theme: "auto", // Automatic theme detection
-  autoDarkTheme : true, //Auto switch to dark theme
+  autoDarkTheme: true, //Auto switch to dark theme
   // App root data
   data() {
     return {
@@ -27,13 +46,24 @@ var app = new Framework7({
   },
   on: {
     // each object key means same name event handler
-    pageInit: function (page) {
+    pageInit: function (page) {         
+           
       var self = this;
       // do something on page init
+      // Create PWA sheet
+      self.sheet.create({
+        el: ".pwa-sheet-swipe-to-close",
+        swipeToClose: true,
+        closeByOutsideClick: true,
+        push: true,
+        backdrop: true,
+      });
+
+      //Add "intro" class to all paragraphs
       // Create swipe-to-close Sheet
       self.sheet.create({
-        el: ".demo-sheet-swipe-to-close",        
-        swipeToClose: true,        
+        el: ".demo-sheet-swipe-to-close",
+        swipeToClose: true,
         closeByOutsideClick: true,
         push: true,
         backdrop: true,
@@ -42,11 +72,11 @@ var app = new Framework7({
   },
 });
 
-$$('#btnSent').on('click', function (e) {
- console.log("Submit Button is Clicked!!");
- var option = $$("#selectmenu").val();
- var num = $$("#number").val();
- url = "https://wa.me/" + option + num;
- console.log("URL : " + url);
- window.open(url, "_top");
+$$("#btnSent").on("click", function (e) {
+  console.log("Submit Button is Clicked!!");
+  var option = $$("#selectmenu").val();
+  var num = $$("#number").val();
+  url = "https://wa.me/" + option + num;
+  console.log("URL : " + url);
+  window.open(url, "_top");
 });
