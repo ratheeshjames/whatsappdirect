35b05f7aebf95254ab01f79ef159a8a771c4fba4
ratheeshjames <54690349+ratheeshjames@users.noreply.github.com>
2020-05-06 18:46:40 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,9 +21,9 @@ var app = new Framework7({
   // App routes
   routes: routes,
   // Register service worker (optional)
-  serviceWorker: {
+  /*serviceWorker: {
     path: "/service-worker.js",
-  },
+  },*/
   on: {
     // each object key means same name event handler
     pageInit: function (page) {
@@ -81,18 +81,17 @@ window.addEventListener("beforeinstallprompt", (e) => {
   });
 });
 */
-/*
+
 window.addEventListener("load", (e) => {
   registerSW();
 });
 async function registerSW() {
   if ("serviceWorker" in navigator) {    
     try {
-      await navigator.serviceWorker.register("service-worker.js");
+      await navigator.serviceWorker.register("/service-worker.js");
     } catch (e) {
       alert("ServiceWorker registration failed. Sorry about that.");
     }
   } else {
   }
 }
-*/
